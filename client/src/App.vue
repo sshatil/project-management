e1f142174e891d83bb457e00baca48c229330c1c
sshatil <mutasimshatil@hotmail.com>
2023-06-10 20:15:50 +0800

    add dark mode funtionality

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main>
+  <main
+    class="flex justify-between text-black dark:text-white dark:bg-black min-h-screen"
+  >
+    <Sidebar />
     <RouterView />
   </main>
 </template>
+
+<script setup lang="ts">
+import Sidebar from "./components/Sidebar.vue";
+</script>
 
 <style scoped></style>
