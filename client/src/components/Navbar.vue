81fa6a1e94a0fc9c494f9bfa29a21ec44ab89d68
sshatil <mutasimshatil@hotmail.com>
2023-08-04 04:17:27 +0800

    design hero section

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,25 +1,31 @@
 <template>
-  <div class="flex justify-between p-3 fixed left-0 right-0 max-w-5xl mx-auto">
-    <RouterLink to="/">Logo</RouterLink>
-    <div class="">
-      <button @click="toggleDark()" class="cursor-pointer">
-        <div
-          v-if="isDark"
-          class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-        >
-          <MoonIcon
-            class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-          />
-        </div>
-        <div
-          v-else
-          class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-        >
-          <SunIcon
-            class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-          />
-        </div>
-      </button>
+  <div class="fixed left-0 right-0">
+    <div class="flex justify-between items-center p-3 max-w-5xl mx-auto">
+      <div class="flex items-center gap-3">
+        <p class="text-2xl mr-6"><RouterLink to="/">Logo</RouterLink></p>
+        <p><RouterLink :to="{ name: 'project' }">Project</RouterLink></p>
+        <p><RouterLink to="/">Log in</RouterLink></p>
+      </div>
+      <div class="">
+        <button @click="toggleDark()" class="cursor-pointer">
+          <div
+            v-if="isDark"
+            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+          >
+            <MoonIcon
+              class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+            />
+          </div>
+          <div
+            v-else
+            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+          >
+            <SunIcon
+              class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+            />
+          </div>
+        </button>
+      </div>
     </div>
   </div>
 </template>
--- client/src/pages/HomeView.vue
+++ client/src/pages/HomeView.vue
@@ -1,7 +1,42 @@
 <template>
-  <div class="text-3xl font-bold underline">Home</div>
+  <div class="max-w-5xl mx-auto px-6">
+    <Navbar />
+    <div class="pt-24 md:pt-20 md:flex justify-evenly items-center space-y-3">
+      <div
+        class="md:basis-2/4 space-y-3 text-center max-w-sm mx-auto md:max-w-xl"
+      >
+        <h1 class="md:text-3xl text-2xl">
+          Manage Your all Projects. Unite Your Tasks, Teammates, and Tools
+        </h1>
+        <p>Everything in One Place, Wherever Your Team Is</p>
+        <div class="">
+          <RouterLink :to="{ name: 'register' }">
+            <button
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Sign Up for free account
+            </button>
+          </RouterLink>
+        </div>
+      </div>
+      <div class="md:basis-2/4 max-w-sm mx-auto md:max-w-2xl">
+        <img src="/project-work.svg" alt="" class="w-full h-full" />
+      </div>
+    </div>
+    <div class="mt-12">
+      <h1 class="text-center">Features</h1>
+      <div class="">
+        <h3>Project List</h3>
+        <div class="max-w-xl mx-auto">
+          <img src="/project-page.png" alt="" class="w-full h-full" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navbar from "../components/Navbar.vue";
+</script>
 
 <style scoped></style>
